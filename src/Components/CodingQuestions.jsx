0f4d9aa662d4ad26b3a8dcdc94e345c7be25ba6e
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-05-02 18:44:30 +0530

    updates of 2 may

--- src/Components/CodingQuestions.jsx
+++ src/Components/CodingQuestions.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const FrontendPrograms = () => {
   return (
     <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Javascript Questions</h2>
-      <div className="p-4 grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 ">
+      <h2 className="mb-4 text-2xl font-bold">Javascript Questions</h2>
+      <div className="grid grid-cols-1 gap-2 p-4 sm:grid-cols-2 lg:grid-cols-3 ">
         {frontendData.map((obj, id) => (
           <div key={id} className="mb-6 ">
-            <ul className="list-disc list-inside rounded-sm gap-2 flex flex-col  p-4 ">
+            <ul className="flex flex-col gap-2 p-4 list-disc list-inside rounded-sm ">
               <Link to={`/interview-preparation/${obj.slug}/SingleQ/${obj.id}`}>
-                <li className="mb-1 px-3 py-5 rounded-md bg-white border-r-8 border-r-[#14324d] font-semibold text-[#063b69] text-sm pl-3">
+                <li className="mb-1 px-3 py-5 rounded-md bg-white border-r-8 shadow-md border-r-[#14324d] font-semibold text-[#063b69] text-sm pl-3">
                   {obj.title}
                 </li>
               </Link>
@@ -23,15 +23,3 @@ const FrontendPrograms = () => {
 };
 
 export default FrontendPrograms;
-
-{
-  /* <ul className="list-disc list-inside rounded-sm gap-2 flex flex-col  p-4 ">
-  {set.map((question, idx) => (
-    <li
-      key={idx}
-      className="mb-1 px-3 py-5 rounded-md bg-white border-r-8 border-r-[#14324d] font-semibold text-[#063b69] text-sm pl-3">
-      {question}
-    </li>
-  ))}
-</ul>; */
-}
--- src/Components/SingleQuestion.jsx
+++ src/Components/SingleQuestion.jsx
@@ -10,23 +10,33 @@ const SingleQuestion = () => {
   );
 
   return (
-    <div className=" ">
-      <div className="  flex items-center justify-center">
+    <div className="">
+      <div className="flex items-center justify-center ">
         {frontendObj?.map((obj, id) => (
           <div key={id} className="w-1/2 py-3">
             <div>
-              <h1 className="text-4xl font-bold py-5 flex justify-center text-[#e32074]">
+              <h1 className="text-4xl font-bold pt-5 flex justify-center text-[#000000]">
                 {obj?.title}
               </h1>
               <p className="text-2xl text-[#637c87] py-5 px-3 flex justify-center items-center self-center">
                 {obj?.desc}
               </p>
             </div>
-            <span className="text-3xl font-bold text-amber-300 flex justify-center">
-              {obj?.Sourcecode}
-            </span>
-            <div className="text-3xl font-bold text-blue-950 flex justify-center py-5">
-              {obj?.output}
+            <div className="mt-6">
+              <h2 className="mb-2 text-xl font-semibold text-gray-700">
+                Source Code
+              </h2>
+              <pre className="p-4 font-mono text-sm text-gray-800 break-words whitespace-pre-wrap bg-gray-100 rounded-md">
+                <code>{obj?.Sourcecode.replace(/^Source Code\s*:/, "")}</code>
+              </pre>
+            </div>
+            <div className="mt-6">
+              <h2 className="mb-2 text-xl font-semibold text-gray-700">
+                Output
+              </h2>
+              <pre className="p-4 font-mono text-sm text-gray-800 break-words whitespace-pre-wrap bg-gray-100 rounded-md">
+                <code>{obj?.output.replace(/^Output\s*:/, "")}</code>
+              </pre>
             </div>
           </div>
         ))}
